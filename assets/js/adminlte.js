e43cd30fe715b03a1d15a04428de7c91e3074d86
TraXXX86 <maxime.jaouen@gmail.com>
2026-02-10 12:05:49 +0100

    chore: update php and js dependencies

--- assets/js/adminlte.js
+++ assets/js/adminlte.js
@@ -1,9 +1,7 @@
 import '../scss/adminlte.scss';
 
 import 'admin-lte/dist/js/adminlte';
-import 'admin-lte/plugins/popper/popper.min';
-import 'admin-lte/plugins/bootstrap/js/bootstrap.min';
-import 'admin-lte/plugins/bootstrap/js/bootstrap.bundle.min';
+import '@popperjs/core/dist/umd/popper.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-import 'jquery/dist/jquery.min';
-
+import 'jquery';
